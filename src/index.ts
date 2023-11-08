192e8321050c80d59e57c00af22558ba5503aa9d
And111rey <samu111ray@gmail.com>
2023-11-08 17:30:48 +0200

    add h1

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!))))Hello new fixed port || 5000 new message')
+  res.send(<h1>Hello Oksanutka</h1>)
 })
 
 app.listen(port, () => {
