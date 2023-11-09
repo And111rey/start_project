b4ed6fee86193ab511be9465487451b3fd3f6692
And111rey <samu111ray@gmail.com>
2023-11-09 16:48:59 +0200

    add get, post, put, del methods

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,90 @@
 // const express = require('express')
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
 const app = express();
+
 const port = process.env.PORT || 5000;
 
-type productsType = Array<{ id: Number; value: string }>;
+type productsType = Array<{ id: Number; title: string }>;
+type addressesType = Array<{ id: Number; value: string }>;
 
-const products = [{ title: "tomato" }, { title: "orange" }];
-const addresses: productsType = [
+const products: productsType = [
+  { id: 1, title: "tomato" },
+  { id: 2, title: "orange" },
+];
+const addresses: addressesType = [
   { id: 1, value: "Myru avenu, 109" },
   { id: 2, value: "Kamenetska, 98" },
 ];
+
+const parserMiddleware = bodyParser()
+app.use(parserMiddleware)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World OKSANUTKA");
 });
 app.get("/products", (req: Request, res: Response) => {
-
-  if(req.query.title){
-    let searchString = req.query.title.toString()
-    res.send(products.filter(p => p.title.indexOf(searchString) > -1 )).sendStatus(200)
+  if (req.query.title) {
+    let searchString = req.query.title.toString();
+    res
+      .send(products.filter((p) => p.title.indexOf(searchString) > -1))
+      .sendStatus(200);
   }
   res.send(`you should select from this list ${products}`);
 });
 
-app.get("/product/:productTitle", (req: Request, res: Response) => {
-  let product = products.find((p) => p.title === req.params.productTitle);
+app.get("/products/:id", (req: Request, res: Response) => {
+  let product = products.find((p) => p.id === +req.params.id);
   if (product) {
     res.send(product).send(200);
   } else {
     res.sendStatus(404);
     res.send("Bad request");
   }
+});
+
+app.delete("/products/:id", (req: Request, res: Response) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === +req.params.id) {
+      products.splice(i, 1);
+      res.sendStatus(204);
+      return;
+    }
+  }
+  res.sendStatus(404);
+  res.send("Bad request");
+});
+
+app.post("/products", (req: Request, res: Response) => {
+  const newProduct = { id: +new Date(), title:  req.body.title};
+  console.log(newProduct)
+  if(newProduct) {
+    products.push(newProduct);
+    res.status(201).send(products);
+  } else {
+    res.status(404).send("Bad request")
+  }
+ 
+});
+app.put("/products/:id", (req: Request, res: Response) => {
+
+   if (req.params.id) {
+     for (let i = 0; i < products.length; i++) {
+
+      if (+req.params.id === products[i].id) {
+        products[i].title = req.body.title
+        const result = {products, changed: products[i]}
+        res.status(200).send(result);
+        return;
+      }
+     }
+   } else {
+     res.status(404);
+   }
+  
+
+ 
 });
 
 app.get("/addresses", (req: Request, res: Response) => {
